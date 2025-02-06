08d54a53191f2ee2d5995ea14ca5c97c860fbdc1
Dinçer Tekin <dincertekin@protonmail.com>
2025-02-06 23:08:20 +0300

    Update install_apps.bat

--- windows/install_apps.bat
+++ windows/install_apps.bat
@@ -1,1 +1,28 @@
-winget install --id=Microsoft.PowerToys -e  && winget install --id=Google.Chrome -e  && winget install --id=voidtools.Everything -e  && winget install --id=EpicGames.EpicGamesLauncher -e  && winget install --id=qBittorrent.qBittorrent -e  && winget install --id=Spotify.Spotify -e  && winget install --id=Microsoft.VisualStudioCode -e --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"'  && winget install --id=VideoLAN.VLC -e  && winget install --id=RARLab.WinRAR -e  && winget install --id=Valve.Steam -e  && winget install --id=OBSProject.OBSStudio -e  && winget install --id=Git.Git -e && winget install --id=Obsidian.Obsidian -e  && winget install --id=Google.AndroidStudio  -e && winget install --id=Microsoft.VisualStudio.2022.Community  -e && winget install --id=Docker.DockerDesktop  -e
+@echo off
+set LOGFILE=%USERPROFILE%\Desktop\winget_install.log
+
+echo Starting installation... > %LOGFILE%
+echo. >> %LOGFILE%
+
+winget install --id=Microsoft.PowerToys -e >> %LOGFILE% 2>&1
+winget install --id=Mozilla.Firefox -e >> %LOGFILE% 2>&1
+winget install --id=voidtools.Everything -e >> %LOGFILE% 2>&1
+winget install --id=EpicGames.EpicGamesLauncher -e >> %LOGFILE% 2>&1
+winget install --id=qBittorrent.qBittorrent -e >> %LOGFILE% 2>&1
+winget install --id=Spotify.Spotify -e >> %LOGFILE% 2>&1
+winget install --id=Microsoft.VisualStudioCode -e --override "/SILENT /mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath" >> %LOGFILE% 2>&1
+winget install --id=VideoLAN.VLC -e --override "/S" >> %LOGFILE% 2>&1
+winget install --id=M2Team.NanaZip -e >> %LOGFILE% 2>&1
+winget install --id=Valve.Steam -e >> %LOGFILE% 2>&1
+winget install --id=OBSProject.OBSStudio -e >> %LOGFILE% 2>&1
+winget install --id=Git.Git -e --override "/VERYSILENT /NORESTART" >> %LOGFILE% 2>&1
+winget install --id=Obsidian.Obsidian -e >> %LOGFILE% 2>&1
+winget install --id=Google.AndroidStudio -e >> %LOGFILE% 2>&1
+winget install --id=Microsoft.VisualStudio.2022.Community -e >> %LOGFILE% 2>&1
+winget install --id=Docker.DockerDesktop -e --override "/quiet" >> %LOGFILE% 2>&1
+
+echo. >> %LOGFILE%
+echo Installation completed! Check for errors in the log file: %LOGFILE% >> %LOGFILE%
+
+echo Installation completed! Check the log at: %LOGFILE%
+pause
