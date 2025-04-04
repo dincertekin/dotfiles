cb2f1efc251aa3feb9de14cb8b5e4828437e67b9
Dinçer Tekin <dincertekin@protonmail.com>
2025-04-04 21:10:05 +0300

    Update install_apps.bat

--- windows/install_apps.bat
+++ windows/install_apps.bat
@@ -1,10 +1,10 @@
-winget install --id=Microsoft.PowerToys -e
 winget install --id=Google.Chrome -e
+winget install --id=VideoLAN.VLC -e
+winget install --id=RARLab.WinRAR -e
 winget install --id=Spotify.Spotify -e
 winget install --id=voidtools.Everything -e
 winget install --id=qBittorrent.qBittorrent -e
-winget install --id=VideoLAN.VLC -e
-winget install --id=RARLab.WinRAR -e
+winget install --id=Microsoft.PowerToys -e
 winget install --id=Spotify.Spotify -e
 winget install --id=Microsoft.VisualStudioCode -e --override "/SILENT /mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
 winget install --id=EpicGames.EpicGamesLauncher -e
@@ -15,3 +15,4 @@ winget install --id=Obsidian.Obsidian -e
 winget install --id=Google.AndroidStudio -e
 winget install --id=Microsoft.VisualStudio.2022.Community -e
 winget install --id=Docker.DockerDesktop -e
+winget install --id=Skillbrains.Lightshot -e
