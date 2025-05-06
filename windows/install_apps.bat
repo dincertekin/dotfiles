199a44e8f21bf4d762b9360503b5db67690dd283
Dinçer Tekin <dincertekin@protonmail.com>
2025-05-06 19:39:29 +0300

    Update install_apps.bat

--- windows/install_apps.bat
+++ windows/install_apps.bat
@@ -5,7 +5,6 @@ winget install --id=Spotify.Spotify -e
 winget install --id=voidtools.Everything -e
 winget install --id=qBittorrent.qBittorrent -e
 winget install --id=Microsoft.PowerToys -e
-winget install --id=Spotify.Spotify -e
 winget install --id=Microsoft.VisualStudioCode -e --override "/SILENT /mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
 winget install --id=OBSProject.OBSStudio -e
 winget install --id=Git.Git -e
@@ -13,6 +12,5 @@ winget install --id=Notion.Notion -e
 winget install --id=Google.AndroidStudio -e
 winget install --id=Microsoft.VisualStudio.2022.Community -e
 winget install --id=WinSCP.WinSCP -e
-winget install --id=JetBrains.Toolbox -e
 wsl --install
 winget install --id=Docker.DockerDesktop -e
