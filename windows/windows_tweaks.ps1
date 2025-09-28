3ebeb0f264b4e5b9c6abe68ffd6d5dbeb938d11b
Dinçer Tekin <dincertekin@protonmail.com>
2025-09-28 21:30:47 +0300

    Update windows_tweaks.ps1

--- windows/windows_tweaks.ps1
+++ windows/windows_tweaks.ps1
@@ -1,17 +1,10 @@
-# Windows 11 (My) Privacy Settings Script
-# For fresh installations
-# Made by https://github.com/dincertekin
-
 # Disable AutoPlay for USB
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 1
 
 # Network & Internet
 $dnsServers = "9.9.9.9","149.112.112.112" # Quad9 DNS
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServers
-# Same DNS Servers for Wi-Fi
 Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses $dnsServers
-# Sshh! Private Mode
-Set-NetConnectionProfile -InterfaceAlias "Wi-Fi" -NetworkCategory Private
 
 # Debloating Windows
 Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage # Cortana
@@ -28,7 +21,7 @@ Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.GetStarted* | Remove-AppxPackage
 
-# Diagnostics & Feedback
+# Disable Microsoft Telemetry
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0
 
 # Must Have Alias for Python 3
