f4118ef58e1e784c562cfef4a0654b718a1708b7
Dinçer Tekin <dincertekin@protonmail.com>
2026-01-29 00:06:18 +0200

    Reorder extensions installation in extensions.sh

--- .vscode/extensions.sh
+++ .vscode/extensions.sh
@@ -3,7 +3,6 @@ code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
 code --install-extension github.copilot
 code --install-extension github.copilot-chat
-code --install-extension jdinhlife.gruvbox
 code --install-extension ms-python.debugpy
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
@@ -13,5 +12,3 @@ code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension rust-lang.rust-analyzer
 code --install-extension svelte.svelte-vscode
 code --install-extension tamasfe.even-better-toml
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension vscodevim.vim
