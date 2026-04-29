3f911d8bdf19a47d97135f39b235ecadd1893a23
Dinçer Tekin <dincertekin@protonmail.com>
2026-04-29 21:52:16 +0200

    Update install_apps.bat

--- windows/install_apps.bat
+++ windows/install_apps.bat
@@ -1,15 +1,28 @@
+# WSL Installation
+# Note: System restart might be required after this command
 wsl --install
-winget install --id=Google.Chrome -e
+
+# Browser and Daily
+winget install --id=Brave.Brave -e
 winget install --id=VideoLAN.VLC -e
-winget install --id=RARLab.WinRAR -e
-winget install --id=Spotify.Spotify -e
-winget install --id=voidtools.Everything -e
+winget install --id=Spotify.Spotify -e --source msstore
 winget install --id=qBittorrent.qBittorrent -e
+
+# System Tools
+winget install --id=M2Team.NanaZip -e
+winget install --id=voidtools.Everything -e
 winget install --id=Microsoft.PowerToys -e
+winget install --id=WinSCP.WinSCP -e
+
+# Development and Writing
+winget install --id=ZedIndustries.Zed -e
 winget install --id=Microsoft.VisualStudioCode -e --override "/SILENT /mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
-winget install --id=OBSProject.OBSStudio -e
 winget install --id=Obsidian.Obsidian -e
+
+# Heavy Development Suites
 winget install --id=Google.AndroidStudio -e
 winget install --id=Microsoft.VisualStudio.2022.Community -e
-winget install --id=WinSCP.WinSCP -e
-winget install --id=Docker.DockerDesktop -e
+winget install --id=OBSProject.OBSStudio -e
+
+# Final Maintenance
+winget upgrade --all
