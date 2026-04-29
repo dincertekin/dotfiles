55725e3f2d1dd5e491e45f6acee603fc40ee2adb
Dinçer Tekin <dincertekin@protonmail.com>
2026-04-29 21:59:35 +0200

    Enhance Windows tweaks with telemetry and debloating
    Added telemetry disabling and aggressive debloating features to the script.

--- windows/windows_tweaks.ps1
+++ windows/windows_tweaks.ps1
@@ -1,28 +1,38 @@
-# Disable AutoPlay for USB
+# Disabling Telemetry (Deep)
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0
+# Disable AutoPlay for USB (Security measure against BadUSB)
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 1
 
-# Network & Internet
-$dnsServers = "9.9.9.9","149.112.112.112" # Quad9 DNS
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServers
-Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses $dnsServers
+# Quad9 DNS for Security
+# We use -ErrorAction SilentlyContinue because some devices might not have both Ethernet and Wi-Fi active
+$dnsServers = "9.9.9.9","149.112.112.112"
+Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Set-DnsClientServerAddress -ServerAddresses $dnsServers -ErrorAction SilentlyContinue
 
-# Debloating Windows
-Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage # Cortana
-Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Windows.Photos* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.3DBuilder* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.People* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.News* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingSearch* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.GetStarted* | Remove-AppxPackage
+# Aggressive Debloating (Keeping the System Lean)
+$appsToRemove = @(
+    "Microsoft.549981C3F5F10",  # Cortana
+    "Microsoft.ZuneMusic",      # Groove Music
+    "Microsoft.ZuneVideo",      # Movies & TV
+    "Microsoft.People",
+    "Microsoft.YourPhone",      # Phone Link
+    "Microsoft.News",
+    "Microsoft.BingWeather",
+    "Microsoft.BingSearch",     # Bing search in Start Menu
+    "Microsoft.WindowsMaps",
+    "Microsoft.GetHelp",
+    "Microsoft.GetStarted",
+    "Microsoft.SkypeApp",
+    "Microsoft.Office.OneNote"
+)
 
-# Disable Microsoft Telemetry
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0
+foreach ($app in $appsToRemove) {
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -ErrorAction SilentlyContinue
+}
 
-# Must Have Alias for Python 3
+# Persistent alias for Python (Works for the current session)
 Set-Alias -Name python3 -Value python
+
+# Show file extensions (Vital for security & dev)
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+# Hide Taskbar Meet Now/Chat icons
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value 0
