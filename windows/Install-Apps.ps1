a94b3151961e8001f6b8fe894a6b8045228f3813
Dinçer Tekin <dincertekin@protonmail.com>
2026-05-07 02:10:47 +0200

    Update Install-Apps.ps1

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -4,13 +4,13 @@ wsl --install
 # Browser and Daily
 winget install --id=Brave.Brave -e
 winget install --id=VideoLAN.VLC -e
-winget install --id=Spotify.Spotify -e --source msstore
+winget install --id 9NCBCSZSJRSB -e --source msstore # Spotify
 winget install --id=qBittorrent.qBittorrent -e
 
 # System Tools
-winget install --id=M2Team.NanaZip -e --source msstore
+winget install --id=9N8G7TSCL18R -e --source msstore # NanaZip
 winget install --id=voidtools.Everything -e
-winget install --id=Microsoft.PowerToys -e --source msstore
+winget install --id=XP89DCGQ3K6VLD -e --source msstore # PowerToys
 winget install --id=WinSCP.WinSCP -e
 
 # Development and Writing
